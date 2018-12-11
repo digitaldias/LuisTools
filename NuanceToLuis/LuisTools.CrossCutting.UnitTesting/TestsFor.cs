45a2abdb858dc194ef4de9d27eead96b25891ba5
Pedro G. Dias <pedro@digitaldias.com>
2018-12-11 08:02:09 +0100

    Introduce base-class for Unit Testing

---
+++ NuanceToLuis/LuisTools.CrossCutting.UnitTesting/TestsFor.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using StructureMap.AutoMocking.Moq;
+
+namespace LuisTools.CrossCutting.UnitTesting
+{
+    public abstract class TestsFor<TInstance> where TInstance : class
+    {
+        protected TInstance Instance { get; set; }
+
+        protected MoqAutoMocker<TInstance> AutoMock { get; set; }
+
+        public TestsFor()
+        {
+            AutoMock = new MoqAutoMocker<TInstance>();
+
+            BeforeTestRun();
+
+            Instance = AutoMock.ClassUnderTest;
+        }
+
+        protected virtual void BeforeTestRun(){
+            // No code
+        }
+
+        public Mock<TContract> GetMockFor<TContract>() where TContract : class
+        {
+            return Mock.Get(AutoMock.Get<TContract>());
+        }
+    }
+}
