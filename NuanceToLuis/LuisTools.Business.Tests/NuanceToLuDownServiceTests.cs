931411ec033077e8b3cc18d9d2b755663d0884ac
Pedro G. Dias <pedro@digitaldias.com>
2018-12-11 12:42:55 +0100

    Introduce Unit-Testing for the NuanceToLudownService class

---
+++ NuanceToLuis/LuisTools.Business.Tests/NuanceToLuDownServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using LuisTools.CrossCutting.UnitTesting;
+using LuisTools.Domain.Contracts;
+using Moq;
+using Xunit;
+
+namespace LuisTools.Business.Tests
+{
+    public class NuanceToLuDownServiceTests : TestsFor<NuanceToLuDownService>
+    {
+
+        [Fact]
+        public void Process_ParametersFailToValidate_DoesNotAttemptToFindAnyFiles()
+        {
+            // Arrange
+            var args = new string[0];
+
+            // Act
+            Instance.Process(args);
+
+            // Assert
+            GetMockFor<IFileFinder>().Verify(f => f.Find(Any<string>()), Times.Never());
+        }
+    }
+}
